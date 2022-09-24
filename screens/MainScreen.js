4d1181aa5a47c5bf3ba51b029aaaa9ff03446b92
Shehroz Ahmad <shehrozahmad970@gmail.com>
2022-09-25 03:17:43 +0500

    Merged with Basit and Shehroz

--- screens/MainScreen.js
+++ screens/MainScreen.js
@@ -1,79 +1,121 @@
-
-import React,{ useState, useEffect, useRef } from 'react';
-import { View, StyleSheet, Image, Platform} from 'react-native';
-import Constants from 'expo-constants';
-import IFLA from './images/IFLA.png';
-import * as SecureStore from 'expo-secure-store';
-import { AuthContext } from '../components/context';
-import * as Device from 'expo-device';
-import * as Notifications from 'expo-notifications';
-const axios = require("axios")
+import React, { useState, useEffect, useRef } from "react";
+import { View, StyleSheet, Image, Platform } from "react-native";
+import Constants from "expo-constants";
+import IFLA from "./images/IFLA.png";
+import * as SecureStore from "expo-secure-store";
+import { AuthContext } from "../components/context";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
+const axios = require("axios");
 
 Notifications.setNotificationHandler({
-    handleNotification: async () => ({
-      shouldShowAlert: true,
-      shouldPlaySound: false,
-      shouldSetBadge: false,
-    }),
-  });
-  
-  registerForPushNotificationsAsync = async () => {
-    let token;
-    if (Device.isDevice) {
-      const { status: existingStatus } = await Notifications.getPermissionsAsync();
-      let finalStatus = existingStatus;
-      if (existingStatus !== 'granted') {
-        const { status } = await Notifications.requestPermissionsAsync();
-        finalStatus = status;
-      }
-      if (finalStatus !== 'granted') {
-        alert('Failed to get push token for push notification!');
-        return;
-      }
-      token = (await Notifications.getExpoPushTokenAsync()).data;
-      console.log(token);
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
-      var response = await axios.post("http://192.168.10.8:3000/notifications/token",{token:{value:token}})
-      // this.setState({ expoPushToken: token });
-    } else {
-      alert('Must use physical device for Push Notifications');
+registerForPushNotificationsAsync = async () => {
+  let token;
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
     }
-  
-    if (Platform.OS === 'android') {
-      Notifications.setNotificationChannelAsync('default', {
-        name: 'default',
-        importance: Notifications.AndroidImportance.MAX,
-        vibrationPattern: [0, 250, 250, 250],
-        lightColor: '#FF231F7C',
-      });
+    if (finalStatus !== "granted") {
+      alert("Failed to get push token for push notification!");
+      return;
     }
-    
-   
-     return token;
-   
-   };
-  
-  async function sendPushNotification(expoPushToken) {
-    const message = {
-      to: expoPushToken,
-      sound: 'default',
-      title: 'Original Title',
-      body: 'And here is the body!',
-      data: { someData: 'goes here' },
-    };
-  
-    await fetch('https://exp.host/--/api/v2/push/send', {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        'Accept-encoding': 'gzip, deflate',
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(message),
+    token = (await Notifications.getExpoPushTokenAsync()).data;
+    console.log(token);
+
+    var response = await axios.post(
+      "http://192.168.10.8:3000/notifications/token",
+      { token: { value: token } }
+    );
+    // this.setState({ expoPushToken: token });
+  } else {
+    alert("Must use physical device for Push Notifications");
+  }
+
+  if (Platform.OS === "android") {
+    Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
     });
   }
+
+  return token;
+};
+
+async function sendPushNotification(expoPushToken) {
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Original Title",
+    body: "And here is the body!",
+    data: { someData: "goes here" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+const MainScreen = ({ route, navigation }) => {
+  const [token, setToken] = React.useState();
+  const { signOut } = React.useContext(AuthContext);
+  const [pushToken, setPushToken] = useState(null);
+  const [notification, setNotification] = useState(false);
+  const notificationListener = useRef();
+  const responseListener = useRef();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync().then((token) => setPushToken(token));
+
+    // This listener is fired whenever a notification is received while the app is foregrounded
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    // This listener is fired whenever a user taps on or interacts with a notification (works when app is foregrounded, backgrounded, or killed)
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(
+        notificationListener.current
+      );
+      Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
+  function getValueFor() {
+    let result = SecureStore.getItemAsync("userToken").then((val) =>
+      setToken(val)
+    );
+    // console.log(result)
+    // if (result)
+    // // {
+    //     setToken(result)
+  }
   const deleteToken = () => {
-    // SecureStore.deleteItemAsync("userToken");
+    SecureStore.deleteItemAsync("userToken");
     signOut();
   };
 
@@ -87,30 +129,10 @@ Notifications.setNotificationHandler({
     return false;
   };
 
-  // const request = ()=>{
-  //     console.log(token)
-  //     var obj = {
-  //         method: 'POST',
-  //         withCredentials: true,
-  //         credentials: 'include',
-  //         headers: {
-  //             'Authorization': `Bearer ${token}`
-  //         }
-  //       }
-  //     fetch('http://192.168.0.103:4000/users/testAuth', obj)
-  //     .then(function(res) {
-  //         return res.json();
-  //     })
-  //     .then(function(resJson) {
-  //         console.log(resJson)
-  //         return resJson;
-  //     })
-  // }
   React.useEffect(() => {
     navigation.addListener("focus", () => {
       getValueFor();
-      // if (isTokenExpired()) deleteToken();
-      // request()
+      if (isTokenExpired()) deleteToken();
     });
   }, [navigation]);
   return (
@@ -125,6 +147,7 @@ Notifications.setNotificationHandler({
       />
     </View>
   );
+};
 
 export default MainScreen;
 
