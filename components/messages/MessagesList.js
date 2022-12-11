bed7115b218976892cfba22e14875afa77de9100
abdulbasit34310 <abdulbasit34310@gmail.com>
2022-12-12 03:53:11 +0500

    Chat

--- components/messages/MessagesList.js
+++ components/messages/MessagesList.js
@@ -1,10 +1,14 @@
 import React, { useState, useRef, useEffect } from "react";
 import { ScrollView } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import axios from 'axios';
+import moment from "moment";
+
+const REST_API_LOCAL = "http://192.168.0.100:4000";
 
 import Message from "./Message";
-import socket from "../socket";
 
-const MessagesList = ({ onSwipeToReply, chatMessages, setChatMessages }) => {
+const MessagesList = ({ navigation, onSwipeToReply, chatMessages, setChatMessages, setChatId, personId, setPersonId }) => {
 	// const [messages, setMessages] = useState([
 	// 	{
 	// 		user: 0,
@@ -71,8 +75,53 @@ const MessagesList = ({ onSwipeToReply, chatMessages, setChatMessages }) => {
 	// 	}
 	// }, [setChatMessages])
 
+
+	// console.log("Get Chats Function");
+
+	// let token1 = await SecureStore.getItemAsync("userToken");
+	// const headers = { Authorization: `Bearer ${token1}` };
+
+	// const res = await axios.get(`${REST_API_LOCAL}/users/getUser`, {
+	// 	withCredentials: true,
+	// 	headers: headers,
+	// });
+	// const data = await res.data;
+
+	// const response = await axios.get(`${REST_API_LOCAL}/chat/user/${data.personId._id}`, {
+	// 	withCredentials: true,
+	// 	headers: headers,
+	// });
+	// setChatId(response.data[0]._id);
+
+	// setChatMessages(response.data[0].messages);
+
 	const user = useRef(0);
 	const scrollView = useRef();
+
+	const getChats = async () => {
+		let token1 = await SecureStore.getItemAsync("userToken");
+		const headers = { Authorization: `Bearer ${token1}` };
+
+		const res = await axios.get(`${REST_API_LOCAL}/users/getUser`, {
+			withCredentials: true,
+			headers: headers,
+		});
+		const user = await res.data.personId;
+		setPersonId(user._id);
+		const response = await axios.get(`${REST_API_LOCAL}/chat/user/${user._id}`, {
+			withCredentials: true,
+			headers: headers,
+		});
+		setChatId(response.data[0]._id);
+		setChatMessages(response.data[0].messages);
+		console.log(chatMessages);
+	};
+
+	useEffect(() => {
+		navigation.addListener("focus", () => {
+			getChats();
+		});
+	}, [chatMessages]);
 
 	return (
 		<ScrollView style={{ backgroundColor: "white", flex: 1 }}
@@ -85,9 +134,11 @@ const MessagesList = ({ onSwipeToReply, chatMessages, setChatMessages }) => {
 				<Message
 					key={index}
 					onSwipe={onSwipeToReply}
-					time={message.time}
-					isLeft={message.user !== user.current}
-					message={message.content}
+					time={moment(message.timeSent)
+						.local()
+						.format("hh:mm a, DD/MM/YY")}
+					isLeft={message.senderId !== personId}
+					message={message.msgBody}
 
 				/>
 			))}
--- components/socket.js
+++
@@ -1,3 +0,0 @@
-import  io  from "socket.io-client";
-const socket = io.connect("http://192.168.0.100:4000");
-export default socket;
